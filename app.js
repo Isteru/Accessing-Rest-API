53bfee1e41517ad1dc587b61d6ecc151ff4cb389
Isteru <adaso56@gmail.com>
2020-12-16 23:37:09 +0100

    Testing getting user repos and accesing only part of the json

--- app.js
+++ app.js
@@ -21,7 +21,33 @@ function getUser(username) {
 req.on('error', error => {
   console.error(error)
 })
-req.end()
+}
+function getReposNames(username) {
+  const url = `https://api.github.com/users/${username}/repos`;
+  const https = require('https');
+  const options = {
+    headers: {
+      'user-agent': 'Isteru'
+    }
+  }
+
+  const req = https.get(url, options, res => {
+  console.log(`statusCode: ${res.statusCode}`)
+  let raw = '';
+  res.on('data', d => {
+    raw+=d;
+  })
+  res.on('end', () => {
+    const parsed = JSON.parse(raw);
+    console.log("Total repos:" + parsed.length);
+    for (repo of parsed) {
+      console.log(repo.name);
+    }
+  })
+})
+req.on('error', error => {
+  console.error(error)
+})
 }
 
-getUser('esjmb');
+getReposNames('esjmb');
