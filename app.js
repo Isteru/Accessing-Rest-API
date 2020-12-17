35cbb7187f19ba01aa674f074982bad2c4899fc4
Isteru <adaso56@gmail.com>
2020-12-17 19:05:01 +0100

    Working on getting a list of all people who collaborated with user, need to do authentication to check if it works

--- app.js
+++ app.js
@@ -22,32 +22,71 @@ req.on('error', error => {
   console.error(error)
 })
 }
-function getReposNames(username) {
-  const url = `https://api.github.com/users/${username}/repos`;
+async function getRepos(username) {
+  return new Promise(function(resolve, reject) {
+    const url = `https://api.github.com/users/${username}/repos`;
+    const https = require('https');
+    const options = {
+      headers: {
+        'user-agent': 'Isteru'
+      }
+    }
+    const req = https.get(url, options, res => {
+    console.log(`statusCode: ${res.statusCode}`)
+    let raw = '';
+    res.on('data', d => {
+      raw+=d;
+    })
+    res.on('end', () => {
+      const parsed = JSON.parse(raw);
+      resolve(parsed);
+    })
+  })
+  req.on('error', error => {
+    cosole.log(error);
+    reject(Error(error));
+  })
+});
+}
+async function getAllUserCollaborators(username) {
+  const repos = await getRepos(username);
   const https = require('https');
   const options = {
     headers: {
       'user-agent': 'Isteru'
     }
   }
-
-  const req = https.get(url, options, res => {
-  console.log(`statusCode: ${res.statusCode}`)
-  let raw = '';
-  res.on('data', d => {
-    raw+=d;
-  })
-  res.on('end', () => {
-    const parsed = JSON.parse(raw);
-    console.log("Total repos:" + parsed.length);
-    for (repo of parsed) {
-      console.log(repo.name);
+  let collaborators=[];
+  let promises = [];
+  for (repo of repos) {
+    promises.push(new Promise(function(resolve, reject) {
+      const url = `https://api.github.com/users/${username}/${repo.name}/collaborators`
+      const req = https.get(url, options, res => {
+        console.log(`statusCode: ${res.statusCode}`)
+        if(res.statusCode==200) {
+          let raw = '';
+          res.on('data', d => {
+            raw+=d;
+          })
+          res.on('end', () => {
+            const parsed = JSON.parse(raw);
+              resolve(parsed);
+          })
+        }
+        else {
+          reject();
+        }
+      })
+    }))
+  }
+  Promise.allSettled(promises).then((results) => results.forEach((result) => {
+    if(result.status='fulfilled') {
+      let repoCollaborators = result.value;
+      for(collaborator of repoCollaborators) {
+        collaborators.push(collaborator);
+      }
     }
-  })
-})
-req.on('error', error => {
-  console.error(error)
-})
+  })).then( () => console.log(collaborators.length));
 }
 
-getReposNames('esjmb');
+getAllUserCollaborators('esjmb');
