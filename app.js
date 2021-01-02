6e0715d106bc78204b05d20c297943e608818275
Isteru <adaso56@gmail.com>
2021-01-02 14:49:10 +0100

    Had some problems with push, hopefully works now

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const https = require('https');
 const { Octokit } = require("@octokit/rest");
 const octokit = new Octokit({
   userAgent: 'Isteru',
-  auth: "11dcb732c5b83fc076354ff89f01bc1761e6c565",
+  auth: "token",
 });
 username = 'Isteru';
 octokit.request('GET /users/{username}/repos', {
