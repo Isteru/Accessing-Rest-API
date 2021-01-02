f07f057c7d86948670145d55afc2b38d7c9de3a0
Isteru <adaso56@gmail.com>
2021-01-02 14:47:04 +0100

    Change from manual requests to octokit/rest.js library, much cleaner

--- app.js
+++ app.js
@@ -1,82 +1,19 @@
 const https = require('https');
-const options = {
-  headers: {
-    'user-agent': 'Isteru'
-  }
-}
-function getUser(username) {
-  const url = `https://api.github.com/users/${username}`;
-
-  const req = https.get(url, options, res => {
-  console.log(`statusCode: ${res.statusCode}`)
-  let raw = '';
-  res.on('data', d => {
-    raw+=d;
-  })
-  res.on('end', () => {
-    const parsed = JSON.parse(raw);
-    console.log(parsed);
-  })
-})
-req.on('error', error => {
-  console.error(error)
-})
-}
-//returns a list of all repos of a user
-async function getRepos(username) {
-  return new Promise(function(resolve, reject) {
-    const url = `https://api.github.com/users/${username}/repos`;
-    const req = https.get(url, options, res => {
-    console.log(`statusCode: ${res.statusCode}`)
-    let raw = '';
-    res.on('data', d => {
-      raw+=d;
-    })
-    res.on('end', () => {
-      const parsed = JSON.parse(raw);
-      resolve(parsed);
-    })
-  })
-  req.on('error', error => {
-    cosole.log(error);
-    reject(Error(error));
-  })
+const { Octokit } = require("@octokit/rest");
+const octokit = new Octokit({
+  userAgent: 'Isteru',
+  auth: "11dcb732c5b83fc076354ff89f01bc1761e6c565",
 });
-}
-//Returns a list of all people who collaborated with user
-async function getAllUserCollaborators(username) {
-  const repos = await getRepos(username);
-  let collaborators=[];
-  let promises = [];
-  for (repo of repos) {
-    promises.push(new Promise(function(resolve, reject) {
-      const url = `https://api.github.com/repos/${username}/${repo.name}/collaborators`
-      const req = https.get(url, options, res => {
-        console.log(`statusCode: ${res.statusCode}`)
-        if(res.statusCode==200) {
-          let raw = '';
-          res.on('data', d => {
-            raw+=d;
-          })
-          res.on('end', () => {
-            const parsed = JSON.parse(raw);
-              resolve(parsed);
-          })
-        }
-        else {
-          reject();
-        }
-      })
-    }))
+username = 'Isteru';
+octokit.request('GET /users/{username}/repos', {
+  username: username
+}).then(({data}) => {
+  for (var repo of data) {
+    octokit.repos.listCollaborators({
+      owner:username,
+      repo: repo.name
+    }).then(({data}) => {
+      console.log(data);
+    })
   }
-  await Promise.allSettled(promises).then((results) => results.forEach((result) => {
-    if(result.status='fulfilled') {
-      let repoCollaborators = result.value;
-      for(collaborator of repoCollaborators) {
-        collaborators.push(collaborator);
-      }
-    }
-  })).then( () => console.log(collaborators.length)).catch( () => console.log("couldn't access"));
-  return collaborators;
-}
-getAllUserCollaborators("Isteru").then((res) => {console.log(res);})
+});
