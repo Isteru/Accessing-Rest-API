eab41a17fa51cf448c71b2178acd37fa568915c6
Isteru <adaso56@gmail.com>
2021-01-03 22:00:29 +0100

    Trying out d3 and hosting a web server using express, d3 code taken from lecture example

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
+var http = require('http');
+var path = require('path');
 const https = require('https');
 const { Octokit } = require("@octokit/rest");
 const octokit = new Octokit({
   userAgent: 'Isteru',
   auth: "token",
 });
-username = 'Isteru';
-octokit.request('GET /users/{username}/repos', {
-  username: username
-}).then(({data}) => {
-  for (var repo of data) {
-    octokit.repos.listCollaborators({
-      owner:username,
-      repo: repo.name
-    }).then(({data}) => {
-      console.log(data);
-    })
-  }
-});
+// Some constances
+const express = require('express')
+const app = express()
+const port = 3000
+// Setup static folder
+app.use(express.static(__dirname + "/static/"));
+// rendering html file.
+app.get('/', (req, res) => {
+res.sendFile(path.join(__dirname + '/index.html'));
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
