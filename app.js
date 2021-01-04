01899da89ea236f72b3db543f27de398ae60f61f
Isteru <adaso56@gmail.com>
2021-01-04 17:43:34 +0100

    Added a prototype function for getting languages used by user,need to process the data into a proper from for d3

--- app.js
+++ app.js
@@ -2,6 +2,24 @@ var http = require('http');
 var path = require('path');
 const https = require('https');
 const { Octokit } = require("@octokit/rest");
+
+class languageNode {
+  constructor(languageName) {
+    this.connectionList = new Map();
+    this.totalBytes = 0;
+    this.languageName = languageName;
+  }
+  addLanguage(language) {
+    if(!this.connectionList.has(language)) {
+      this.connectionList.set(language, {connectionCount: 1});
+    }
+    else {
+      let currentCount = this.connectionList.get(language).connectionCount;
+      this.connectionList.set(language, {connectionCount: currentCount+1});
+    }
+  }
+}
+
 const octokit = new Octokit({
   userAgent: 'Isteru',
   auth: "token",
@@ -42,3 +60,34 @@ app.get('/data.json', (req, res) => {
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+let languages = new Map();
+octokit.repos.listForUser({
+  username:username,
+}).then (({data}) => {
+  for(var repo of data) {
+    octokit.repos.listLanguages({
+      owner:username,
+      repo:repo.name,
+    }).then (({data}) => {
+      for (var language in data) {
+        let node;
+        if (!languages.has(language)) {
+          node = new languageNode(language);
+        }
+        else {
+          node = languages.get(language);
+        }
+        node.totalBytes += data[language];
+        languages.set(language,node);
+      }
+      for(var firstLanguage in data) {
+        for (var secondLanguage in data) {
+          if (firstLanguage !== secondLanguage) {
+            languages.get(firstLanguage).addLanguage(secondLanguage);
+          }
+        }
+      }
+    })
+  }
+})
